cfcb35ef05c0dada74ba837b48bfc61771ca1362
Javier Rojas <jare_123@hotmail.es>
2025-06-06 22:03:20 -0500

    .env example

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,18 +2,21 @@
 
 echo "Esperando a que la base de datos esté disponible..."
 
-until php -r "try {
+until php -r "
+try {
     new PDO(
         getenv('DB_CONNECTION') . ':host=' . getenv('DB_HOST') . ';port=' . getenv('DB_PORT') . ';dbname=' . getenv('DB_DATABASE'),
         getenv('DB_USERNAME'),
         getenv('DB_PASSWORD')
     );
-    echo 'Conexión a la base de datos exitosa.';
+    echo 'Conexión exitosa a la base de datos.' . PHP_EOL;
+    exit(0);
 } catch (Exception \$e) {
-    echo 'Esperando conexión...';
+    echo 'Error de conexión: ' . \$e->getMessage() . PHP_EOL;
     exit(1);
-}";
-do
+}
+"; do
+  echo "Reintentando conexión..."
   sleep 3
 done
 
